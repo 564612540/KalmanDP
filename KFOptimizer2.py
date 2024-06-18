55c2e1e96b97ec78cf63ee8849579a9a78daa001
Zhiqi Bu <woodyx218@gmail.com>
2024-06-17 21:30:30 -0400

    Update KFOptimizer2.py

--- KFOptimizer2.py
+++ KFOptimizer2.py
@@ -3,7 +3,7 @@ from torch.optim.optimizer import Optimizer, required
 import torch.autograd as ta
 
 class KFOptimizer2(Optimizer):
-    def __init__(self, params, optimizer:Optimizer, sigma_H=0, sigma_g=1e-5):
+    def __init__(self, params, optimizer:Optimizer, sigma_H=0, sigma_g=1e-5, gamma=0.1):
         '''
         # wrapping up the optimizer with
         optimizer = KFOptimizer(model.parameters(), optimizer, sigma_H, sigma_g)
@@ -11,7 +11,7 @@ class KFOptimizer2(Optimizer):
         if t % acc_step == 0 and hasattr(optimizer, 'prestep'):
             optimizer.prestep()
         '''
-        defaults = dict(sigma_H=sigma_H, sigma_g=sigma_g)
+        defaults = dict(sigma_H=sigma_H, sigma_g=sigma_g, gamma=gamma)
         self.optimizer = optimizer
         # if nesterov and (momentum <= 0 or dampening != 0):
             # raise ValueError("Nesterov momentum requires a momentum and zero dampening")
@@ -19,7 +19,20 @@ class KFOptimizer2(Optimizer):
 
     def __setstate__(self, state):
         super(KFOptimizer2, self).__setstate__(state)
-
+ 
+    def prestep(self):
+        for group in self.param_groups:
+            gamma = group['gamma']
+            for p in group['params']:
+                if not p.requires_grad:
+                    continue
+                
+                p.x_t=p.data.clone()
+                
+                if 'x_t_minus1' not in self.state[p]:
+                    continue
+                p.data=(1-gamma)*p.x_t+gamma*p.x_t_minus1
+                        
     def step(self, closure=None):
         """Performs a single optimization step.
         Arguments:
@@ -30,19 +43,14 @@ class KFOptimizer2(Optimizer):
         for group in self.param_groups:
             sigma_g = group['sigma_g']
             sigma_H = group['sigma_H']
+            gamma = group['gamma']
             for p in group['params']:
                 has_private_grad = False
                 if not p.requires_grad:
                     continue
                 else:
-                    p.previous_data=p.data.clone()
-                if hasattr(p, 'private_grad'):
-                    grad = p.private_grad
-                    has_private_grad = True
-                elif p.grad is not None:
-                    grad = p.grad
-                else:
-                    continue
+                    p.x_t_minus1=p.data.clone()
+
                 if 'kf_beta_t' not in self.state[p]:
                     self.state[p]['kf_beta_t'] = sigma_g**2 #??? #t=0
                     # self.state[p]['kf_d_t'] = torch.zeros_like(p.data).to(p.data)
@@ -51,15 +59,15 @@ class KFOptimizer2(Optimizer):
                 k_t = beta_t/(beta_t + sigma_g**2 - sigma_H**2) #t=0
                 self.state[p]['kf_beta_t'] = (1-k_t)*beta_t #t=0
                 
-                self.state[p]['kf_m_t'].lerp_(grad, weight = (1-k_t)/k_t) #t>=0
-        
+                self.state[p]['kf_m_t']=(1-k_t)*self.state[p]['kf_m_t']+(1-k_t)/gamma*p.grad_t_plus+(k_t-(1-k_t)/gamma)*p.grad_t
+                
                 if has_private_grad:
                     p.private_grad = self.state[p]['kf_m_t'].clone().to(p.data)
                 else:
                     p.grad = self.state[p]['kf_m_t'].clone().to(p.data)
 
-        for group, group_orig in zip(self.param_groups,self.optimizer.param_groups):
-            group['lr']=(1-(1-k_t)/k_t)*group_orig['lr']
+        # for group, group_orig in zip(self.param_groups,self.optimizer.param_groups):
+        #     group['lr']=(1-(1-k_t)/k_t)*group_orig['lr']
 
         loss = self.optimizer.step(closure)
 
