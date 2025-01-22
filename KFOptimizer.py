fix memory leakage

KFOptimizer.py
@@ -1,5 +1,5 @@
 import torch
-from torch.optim.optimizer import Optimizer, required
+from torch.optim.optimizer import Optimizer, required, _use_grad_for_differentiable
 import torch.autograd as ta
 from collections import defaultdict
 from typing import Callable, List, Optional, Union
@@ -116,7 +116,7 @@ def wrap_optimizer(optimizer, kappa = 0.7, gamma = 0.5):
                     closure()
                 else:
                     loss = closure()
-            with torch.no_grad:
+            with torch.no_grad():
                 for group in self.param_groups:
                     # gamma = group['gamma']
                     for p in group['params']:
@@ -132,7 +132,7 @@ def wrap_optimizer(optimizer, kappa = 0.7, gamma = 0.5):
                                 p.grad.div_(self.scaling_factor)
             return loss
                 
-
+        @_use_grad_for_differentiable
         def step(self, closure=None):
             """Performs a single optimization step.
             Arguments:
@@ -143,52 +143,50 @@ def wrap_optimizer(optimizer, kappa = 0.7, gamma = 0.5):
             kappa = self.defaults['kappa']
             tmp_states = []
             first_step = False
-            with torch.no_grad():
-                for group in self.param_groups:
-                    # kappa = group['kappa']
-                    for p in group['params']:
-                        has_private_grad = False
-                        if hasattr(p, 'private_grad'):
-                            grad = p.private_grad
-                            has_private_grad = True
-                        elif p.grad is not None:
-                            grad = p.grad
-                        else:
-                            continue
-                        if self.compute_grad:
-                            grad.div_(1+1/self.scaling_factor)
-                        state = self.state[p]
-                        if 'kf_d_t' not in state:
-                            state = dict()
-                            first_step = True
-                            state['kf_d_t'] = torch.zeros_like(p.data).to(p.data)
-                            state['kf_m_t'] = grad.clone().to(p.data)
-                        state['kf_m_t'].lerp_(grad, weight = kappa)
-                        if has_private_grad:
-                            p.private_grad = state['kf_m_t'].clone().to(p.data)
-                        else:
-                            p.grad = state['kf_m_t'].clone().to(p.data)
-                            scaling_factor += p.grad.norm().pow(2)
-                        state['kf_d_t'] = -p.data.clone().to(p.data)
-                        if first_step:
-                            tmp_states.append(state)
-                if scaling_factor > 0 and not has_private_grad:
-                    scaling_factor = scaling_factor.sqrt()
-                    for group in self.param_groups:
-                        for p in group['params']:
-                            if p.grad is not None:
-                                p.grad.div_(scaling_factor)
-            loss = self.original_optimizer.step(closure)
-            with torch.no_grad():
+            for group in self.param_groups:
+                # kappa = group['kappa']
+                for p in group['params']:
+                    has_private_grad = False
+                    if hasattr(p, 'private_grad'):
+                        grad = p.private_grad
+                        has_private_grad = True
+                    elif p.grad is not None:
+                        grad = p.grad
+                    else:
+                        continue
+                    if self.compute_grad:
+                        grad.div_(1+1/self.scaling_factor)
+                    state = self.state[p]
+                    if 'kf_d_t' not in state:
+                        state = dict()
+                        first_step = True
+                        state['kf_d_t'] = torch.zeros_like(p.data).to(p.data)
+                        state['kf_m_t'] = grad.clone().to(p.data)
+                    state['kf_m_t'].lerp_(grad, weight = kappa)
+                    if has_private_grad:
+                        p.private_grad = state['kf_m_t'].clone().to(p.data)
+                    else:
+                        p.grad = state['kf_m_t'].clone().to(p.data)
+                        scaling_factor += p.grad.norm().pow(2)
+                    state['kf_d_t'] = -p.data.clone().to(p.data)
+                    if first_step:
+                        tmp_states.append(state)
+            if scaling_factor > 0 and not has_private_grad:
+                scaling_factor = scaling_factor.sqrt()
                 for group in self.param_groups:
                     for p in group['params']:
                         if p.grad is not None:
-                            if first_step:
-                                tmp_state = tmp_states.pop(0)
-                                self.state[p]['kf_d_t'] = tmp_state['kf_d_t']
-                                self.state[p]['kf_m_t'] = tmp_state['kf_m_t']
-                                del tmp_state
-                            self.state[p]['kf_d_t'].add_(p.data, alpha = 1)
+                            p.grad.div_(scaling_factor)
+            loss = self.original_optimizer.step(closure)
+            for group in self.param_groups:
+                for p in group['params']:
+                    if p.grad is not None:
+                        if first_step:
+                            tmp_state = tmp_states.pop(0)
+                            self.state[p]['kf_d_t'] = tmp_state['kf_d_t']
+                            self.state[p]['kf_m_t'] = tmp_state['kf_m_t']
+                            del tmp_state
+                        self.state[p]['kf_d_t'].add_(p.data, alpha = 1)
             return loss
         def __repr__(self):
             return self.original_optimizer.__repr__()

run_KFSGD.py
@@ -8,13 +8,11 @@ from fastDP import PrivacyEngine, PrivacyEngine_Distributed_extending
 import argparse
 import warnings
 import gc
-import os
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
-    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
     torch.manual_seed(42)
-    parser = argparse.ArgumentParser(description='DiSK')
+    parser = argparse.ArgumentParser(description='LP DPSGD')
     parser = base_parse_args(parser)
     args = parser.parse_args()
     train_dl, test_dl, model, device, sample_size, acc_step, noise = task_init(args)
@@ -56,13 +54,13 @@ if __name__ == '__main__':
         lrscheduler = None
 
     if args.kf:
-        optimizer = wrap_optimizer(optimizer, kappa=args.kappa, gamma=args.gamma)#KFOptimizer(model.parameters(), optimizer=optimizer, kappa=args.kappa, gamma=args.gamma)
+        optimizer = wrap_optimizer(optimizer=optimizer, kappa=args.kappa, gamma=args.gamma)
     
     criterion = torch.nn.CrossEntropyLoss(reduction='mean')
     if args.clipping:
-        privacy_engine = PrivacyEngine_Distributed_extending(model, noise_multiplier=noise, grad_accum_steps = acc_step, sample_size= sample_size, batch_size=args.bs, epochs= args.epoch, per_sample_clip=args.clipping, torch_seed_is_fixed=True, num_GPUs=1)
-        # privacy_engine = PrivacyEngine(model, noise_multiplier=noise, numerical_stability_constant=1e-3, grad_accum_steps = acc_step, sample_size= sample_size, batch_size=args.bs, epochs= args.epoch, per_sample_clip=args.clipping, torch_seed_is_fixed=False, clipping_fn=args.clipping_fn, clipping_style=args.clipping_style, max_grad_norm=args.clipping_norm)
-        # privacy_engine.attach(optimizer)
+        # privacy_engine = PrivacyEngine_Distributed_extending(model, noise_multiplier=noise, grad_accum_steps = acc_step, sample_size= sample_size, batch_size=args.bs, epochs= args.epoch, per_sample_clip=args.clipping, torch_seed_is_fixed=True, num_GPUs=1)
+        privacy_engine = PrivacyEngine(model, noise_multiplier=noise, numerical_stability_constant=1e-3, grad_accum_steps = acc_step, sample_size= sample_size, batch_size=args.bs, epochs= args.epoch, per_sample_clip=args.clipping, torch_seed_is_fixed=False, clipping_fn=args.clipping_fn, clipping_style=args.clipping_style, max_grad_norm=args.clipping_norm)
+        privacy_engine.attach(optimizer)
         
 
     if args.kf and args.load_path is not None:

train_utils.py
@@ -46,7 +46,7 @@ def train(model, train_dl, optimizer, criterion, log_file, device = 'cpu', epoch
             optimizer.zero_grad()
 
         if (t+1)%(acc_step)== 0 or ((t + 1) == len(train_dl)):
-            print('Epoch: %d:%d Train Loss: %.3f | Acc: %.3f%% (%d/%d)'% (epoch, t+1, train_loss, 100.*correct/total, correct, total))
+            print('Epoch: %d:%d Train Loss: %.3f | Acc: %.3f%% (%d/%d) | Mem_used: %d'% (epoch, t+1, train_loss, 100.*correct/total, correct, total, torch.cuda.memory_allocated(device)))
             if log_frequency>0 and ((t+1)%(acc_step*log_frequency) == 0 or t+1 == len(train_dl)):
                 log_file.update([epoch, t],[100.*correct/total, train_loss])
 def noisy_train(model, train_dl, optimizer, criterion, log_file, device = 'cpu', epoch = -1, noise = 0, log_frequency = -1, acc_step = 1, lr_scheduler = None):